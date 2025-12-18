75c02c11d1cc0164a85ce0a3adcb66dbb5b06ef1
dhruv20047228 <dhruvp04004@gmail.com>
2025-12-18 11:11:31 +0530

    implement welcome section

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.css";
+import { Welcome, Navbar } from "#components";
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <Welcome />
+    </main>
+  );
+}
+
+export default App;
